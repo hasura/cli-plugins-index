83ba2869562bca659a2582fc8bcc577ac8124d67
Samir Talwar <samir.talwar@hasura.io>
2024-08-13 00:41:25 +0200

    Use nix-shell to simplify the ndc-postgres release script.

--- plugins/ndc-postgres/release.sh
+++ plugins/ndc-postgres/release.sh
@@ -1,54 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p gh jq yq
 
 set -e
 set -u
 set -o pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
-
-NIX=false
-if command -v nix > /dev/null; then
-  NIX=true
-fi
-
-if ! GH="$(command -v gh)"; then
-  if $NIX; then
-    GH=( nix run 'nixpkgs#gh' '--' )
-  else
-    echo >&2 'Install the GitHub CLI (gh): https://cli.github.com/'
-    exit 2
-  fi
-fi
-
-function gh {
-  "${GH[@]}" "$@"
-}
-
-if ! JQ="$(command -v jq)"; then
-  if $NIX; then
-    JQ=( nix run 'nixpkgs#jq' '--' )
-  else
-    echo >&2 'Install jq: https://jqlang.github.io/jq/'
-    exit 2
-  fi
-fi
-
-function jq {
-  "${JQ[@]}" "$@"
-}
-
-if ! YQ="$(command -v yq)"; then
-  if $NIX; then
-    YQ=( nix run 'nixpkgs#yq' '--' )
-  else
-    echo >&2 'Install yq: https://kislyuk.github.io/yq/'
-    exit 2
-  fi
-fi
-
-function yq {
-  "${YQ[@]}" "$@"
-}
 
 if [[ $# -ne 1 ]]; then
   echo >&2 "Usage: ${BASH_SOURCE[0]} VERSION"
